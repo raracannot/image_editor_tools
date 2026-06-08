3b7e254b9254dae995c5602a263215e55ba98ef1
RARA <supperdao@qq.com>
2026-06-08 22:39:52 +0800

    fix: v0.3.1 - 统一 Panel 分类名为 Tool

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "Image Editor Master [图像超级工具]",
     "author": "RARA",
-    "version": (0, 3, 0),
+    "version": (0, 3, 1),
     "blender": (4, 2, 0),
     "location": "Image Editor > Sidebar(N-Panel) > Tool",
     "description": "Super Image Toolbox (color, filters, texture optimization)",
@@ -132,7 +132,7 @@ class IMAGEEDITOR_TOOLS_PT_ToolPanel(bpy.types.Panel):
     bl_idname = "IMAGEEDITOR_TOOLS_PT_ToolPanel"
     bl_space_type = 'IMAGE_EDITOR'
     bl_region_type = 'UI'
-    bl_category = "工具"
+    bl_category = "Tool"
 
     @classmethod
     def poll(cls, context):
